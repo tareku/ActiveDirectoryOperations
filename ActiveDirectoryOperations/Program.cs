eee09126b444cdbc822f7e85f61163e45db98673
tareku <tarekbenbrahim1@gmail.com>
2020-07-23 21:30:46 +0100

    init commit

---
+++ ActiveDirectoryOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
